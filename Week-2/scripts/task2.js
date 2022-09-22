fb5c2167573fc90be0e845e4010354b99bac8ee1
Bryce <themaughan1.0@gmail.com>
2022-09-22 13:52:36 -0600

    Updates from class

--- Week-2/scripts/task2.js
+++ Week-2/scripts/task2.js
@@ -1,19 +1,37 @@
 /* Lesson 2 */
 
 /* VARIABLES */
-
+let student = 'images/me.jpg';
 // Step 1: declare and instantiate a variable to hold your name
 document.querySelector('h1').textContent = "Bryce's Bio";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-//document.querySelector('ul').innerHTML = '<li>List Item One</li>'
-
-
+document.querySelector('img').setAttribute('src', student);
+document.querySelector('img').setAttribute('width', 240);
+document.querySelector('img').setAttribute('alt', 'Bryce and Imihaa got Married!!');
 // Step 3: declare and instantiate a variable to hold the current year
+let classes = ['CSE121B', 'REL200C', 'ARCH225', 'ARCH290'];
+console.log(classes);
+//classes [2] = 'WDD230';
+//console.log(classes [2]);
 
+//2 is starting and '1' is how many to take out.
+//classes.shift();
+//document.querySelector('#courses').textContent = classes.join(' ');
+classes.splice(2, 2);
+document.querySelector('#courses').textContent = classes.join(' ');
+//classes.pop();
+//document.querySelector('#courses').textContent = classes.join(' ');
 
 // Step 4: place the value of the current year variable into the HTML file
 
+let currentDateAndTime = Date();
+console.log("It is now "+currentDateAndTime);
+
+let date = new Date();
+
+let year = date.getFullYear(date);
+console.log(year);
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
 
