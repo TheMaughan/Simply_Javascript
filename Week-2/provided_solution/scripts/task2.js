4b599bde8d716a17694038030d063b0998898749
Bryce <mau16009@byui.edu>
2022-09-27 13:12:42 -0600

    w-2 finish & create w-3

--- Week-2/provided_solution/scripts/task2.js
+++ Week-2/provided_solution/scripts/task2.js
@@ -3,80 +3,54 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-let myName = "Bryce Maughan";
+const myName = 'Brother Thayne';
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
 document.querySelector('#name').textContent = myName;
 
 // Step 3: declare and instantiate a variable to hold the current year
-let currentDateAndTime = Date();
-console.log("It is now "+currentDateAndTime);
-
-let date = new Date();
-
-let year = date.getFullYear(date);
-
-console.log(year);
-
+const currentYear = 2020;
 
 // Step 4: place the value of the current year variable into the HTML file
-document.querySelector('#year').textContent = year;
-
-
+document.querySelector('#year').textContent = currentYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-let student = 'images/me.jpg';
+const myPicture = 'images/photo.jpg';
 
 // Step 6: copy your image into the "images" folder
-document.querySelector('img').setAttribute('src', student);
-
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-document.querySelector('img').setAttribute('width', 240);
-document.querySelector('img').setAttribute('alt', 'Bryce and Imihaa got Married!!');
+document.querySelector('img').setAttribute('src', myPicture);
 
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-let classes = ['Ice Cream,', 'Sushi,', 'Raman,', 'Sea Food'];
-console.log(classes);
-
+const favoriteFoods = ['Fettucini', 'Steak', 'Chicken', 'Shrimp'];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector('#food').textContent = classes.join(' ');
-
+document.querySelector('#food').textContent = favoriteFoods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-let newItem = 'Authentic Japaneese Raman';
-
+const anotherFavoriteFood = 'Baked Potato';
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-classes [2] = newItem;
-//console.log(classes [2]);
-
+favoriteFoods.push(anotherFavoriteFood);
 
 // Step 5: repeat Step 2
-document.querySelector('#food').textContent = classes.join(' ');
-
+document.querySelector('#food').textContent = favoriteFoods;
 
 // Step 6: remove the first element in the favorite foods array
-//2 is starting and '1' is how many to take out.
-classes.splice(0, 1);
-
+favoriteFoods.shift();
 
 // Step 7: repeat Step 2
-document.querySelector('#food').textContent = classes.join(' ');
-
+document.querySelector('#food').textContent = favoriteFoods;
 
 // Step 8: remove the last element in the favorite foods array
-classes.pop();
-
+favoriteFoods.pop();
 
 // Step 7: repeat Step 2
-document.querySelector('#food').textContent = classes.join(' ');
+document.querySelector('#food').textContent = favoriteFoods;
 
 
-//classes.shift();
-//document.querySelector('#courses').textContent = classes.join(' ');
