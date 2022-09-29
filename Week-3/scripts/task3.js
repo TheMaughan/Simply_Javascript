b2e00ef8a315ae9f585dfe0eda12b747c7165946
Bryce <mau16009@byui.edu>
2022-09-29 14:12:43 -0600

    Some learning

--- Week-3/scripts/task3.js
+++ Week-3/scripts/task3.js
@@ -24,12 +24,17 @@
 /* BUILT-IN METHODS */
 
 // Step 1: Declare and instantiate a variable of type Date to hold the current date
+let currentDateAndTime = Date();
+console.log("It is now "+currentDateAndTime);
 
 // Step 2: Declare a variable to hold the current year
 
 // Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
 
 // Step 4: Assign the current year variable to an HTML form element with an ID of year
+let date = new Date();
+let year = date.getFullYear(date);
+document.querySelector('#year').textContent = year;
 
 
 /* ARRAY METHODS */
