d8a12958bd64a5c78ac479a1682124ebfde4b207
Bryce <mau16009@byui.edu>
2022-10-09 17:45:23 -0600

    Finished Wee4-4

--- Week-4/Proove/scripts/task4.js
+++ Week-4/Proove/scripts/task4.js
@@ -20,8 +20,8 @@ const myInfo = {
 		{
 		id: 1,
 		city: 'Provo',
-		state: 'Utan',
-		years: 5
+		state: 'Utah',
+		years: '5 years'
 		},
 		{
 		id: 2,
@@ -32,32 +32,32 @@ const myInfo = {
 		{
 		id: 3,
 		city: 'Springville',
-		state: 'Utan',
-		years: 2
+		state: 'Utah',
+		years: '2 years'
 		},
 		{
 		id: 4,
 		city: 'Iona',
 		state: 'Idaho',
-		years: 2
+		years: '2 years'
 		},
 		{
 		id: 5,
 		city: 'Rigby',
 		state: 'Idaho',
-		years: 6
+		years: '6 years'
 		},
 		{
 		id: 6,
 		city: 'Sugar-Salem',
 		state: 'Idaho',
-		years: 5
+		years: '5 years'
 		},
 		{
 		id: 7,
 		city: 'Rexburg',
 		state: 'Idaho',
-		years: 6
+		years: '6 years'
 		},
 	],
 };
@@ -75,17 +75,23 @@ document.querySelector('#photo').setAttribute('alt', myInfo.name);
 document.querySelector('#photo').setAttribute('width', 240);
 
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
-
-
-
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
+myInfo.foods.forEach(foodie => {
+	document.querySelector('#favorite-foods').innerHTML += '<li>' + foodie + '</li>';
+})
 
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
-
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
+myInfo.hobbies.forEach(goodie => {
+	document.querySelector('#hobbies').innerHTML += '<li>' + goodie + '</li>';
+})
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
-
 // Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
+myInfo.Lived.forEach(locations => {
+	document.querySelector('#places-lived').innerHTML += 
+	'<dt>' + locations.city + ', ' + locations.state + '</dt>' +
+	'<dd>' + locations.years + '</dd>';
+})
