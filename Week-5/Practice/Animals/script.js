99f7f7d89b4a1098aa44e033a6a64b0cf4005398
Bryce <mau16009@byui.edu>
2022-10-15 20:58:41 -0600

    update

--- Week-5/Practice/Animals/script.js
+++ Week-5/Practice/Animals/script.js
@@ -24,59 +24,6 @@
 //	  }
 //	}
 //  ];
-//
-//
-//console.log(animals[1].name + ' ' + animals[1].foods.likes[0]);
-//
-//function sortBy() {
-//	document.querySelector('#pets').innerHTML = '';
-//	let sort = document.querySelector('#sortBy').value;
-//	if (sort = 'petNameAscending') {
-//		animals.sort(animal_sort_asc);
-//		outputPets(animals.sort(animal_sort_asc));
-//	} else if (sort == 'petNameDescending'){
-//		outputPets(animals.sort(animal_sort_desc));
-//	}
-//}
-//
-//
-//function animal_sort_asc(a, b) {
-//	if (a.name < b.name) {
-//		return -1;
-//	}
-//	else if (a.name > b.name) {
-//		return 1;
-//	}
-//	else {
-//		return 0;
-//	}
-//}
-//
-//function animal_sort_desc(a, b) {
-//	if (a.name < b.name) {
-//		return 1;
-//	}
-//	else if (a.name > b.name) {
-//		return -1;
-//	}
-//	else {
-//		return 0;
-//	}
-//}
-//
-//function outputPets() {
-//	animals.array.forEach(item => {
-//		let petSection = document.querySelector('#pets');
-//		let petName = document.createElement('h2');
-//		let petSpecies = document.createElement('h3');
-//		petName.textContent = item.name;
-//		petSpecies.textContent = item.Species;
-//		petSection.appendChild(petName);
-//		petSection.appendChild(petSpecies);
-//	});
-//}
-//
-//document.querySelector('#sortBy').addEventListener('change', sortBy);
 
 const purl = 'https://learnwebcode.github.io/json-example/animals-1.json';
 
@@ -89,6 +36,60 @@ async function getPets(purl) {
 		outputPets(data);
 	}
 }
+
+//console.log(animals[1].name + ' ' + animals[1].foods.likes[0]);
+
+function sortBy() {
+	document.querySelector('#pets').innerHTML = '';
+	let sort = document.querySelector('#sortBy').value;
+	if (sort = 'petNameAscending') {
+		purl.sort(animal_sort_asc);
+		outputPets(purl.sort(animal_sort_asc));
+	} else if (sort == 'petNameDescending'){
+		outputPets(purl.sort(animal_sort_desc));
+	}
+}
+
+
+function animal_sort_asc(a, b) {
+	if (a.name < b.name) {
+		return -1;
+	}
+	else if (a.name > b.name) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+function animal_sort_desc(a, b) {
+	if (a.name < b.name) {
+		return 1;
+	}
+	else if (a.name > b.name) {
+		return -1;
+	}
+	else {
+		return 0;
+	}
+}
+
+//function outputPets() {
+//	animals.forEach(item => {
+//		let petSection = document.querySelector('#pets');
+//		let petName = document.createElement('h2');
+//		let petSpecies = document.createElement('h3');
+//		petName.textContent = item.name;
+//		petSpecies.textContent = item.Species;
+//		petSection.appendChild(petName);
+//		petSection.appendChild(petSpecies);
+//	});
+//}
+
+//document.querySelector('#sortBy').addEventListener('change', sortBy);
+
+
 
 function outputPets(data) {
 	data.forEach(item => {
@@ -105,4 +106,4 @@ function outputPets(data) {
 
 getPets(purl);
 
-//document.querySelector('#pets').addEventListener('change', outputPets);
+document.querySelector('#pets').addEventListener('change', outputPets);
